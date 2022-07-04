f50986ae6ef62d962055792bcf83648a4a1372e6
Artem Koton <kotonartem@protonmail.com>
2022-07-04 16:43:19 +0300

    Use unique_ptr for clone (and update tests)

--- include/expression.h
+++ include/expression.h
@@ -3,14 +3,15 @@
 #include "complex.h"
 
 #include <map>
+#include <memory>
 #include <ostream>
 #include <string>
 
 struct Expression
 {
-    virtual Complex eval(std::map<std::string, Complex> const & values = {}) const = 0;
+    virtual Complex eval(const std::map<std::string, Complex> & values = {}) const = 0;
 
-    virtual Expression * clone() const = 0;
+    virtual std::unique_ptr<Expression> clone() const = 0;
 
     friend std::ostream & operator<<(std::ostream & out, const Expression & expression);
 };
